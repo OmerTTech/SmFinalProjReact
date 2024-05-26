e47eb704e27c3eccb488d010156d6574dda56753
HyperOmerJ <omer.tapdiqov@gmail.com>
2024-05-27 01:52:06 +0400

    updated

--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const UserCard = () => {
+const UserCard = ({user}) => {
   return (
-    <div className='d-flex flex-column'>
-      <p></p>
-      <p></p>
-      <img src={} alt="" />
+    <div className='col-4 pt-3 d-flex flex-column usercard'>
+      <p className='text-center fw-bold m-0'>{`${user.first_name} ${user.last_name}`}</p>
+      <p className='text-center'>{user.email}</p>
+      <img src={user.avatar} alt="" />
     </div>
   )
 }
--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,25 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { getUsers } from "../../Services/api";
 import UserCard from "../../Components/UserCard/UserCard";
+import { UserContext } from "../../Services/userContext";
 
 const Users = () => {
-  
-  useEffect(()=>{
+  const { users, setUsers } = useContext(UserContext);
+  useEffect(() => {
     const fetchUsers = async () => {
-      const response = await getUsers(2)
-      setUsers(response.data.data)
-    }
-    fetchUsers()
-    console.log(users);
-  },[])
+      const response = await getUsers(1);
+      setUsers(response.data.data);
+    };
+    fetchUsers();
+  }, []);
   return (
     <div className="container ">
-      <div className="row my-2">
-        {users ? users.map((item) => {
-          return(
-            <UserCard />
-          )
-        }) : null}
+      <div className="row mx-5 p-5 pt-4 g-5">
+        {users ? (
+          users.map((item) => {
+            return <UserCard key={item.id} user={item} />;
+          })
+        ) : (
+          <div class="alert alert-danger" role="alert">
+            Not Users Found!
+          </div>
+        )}
       </div>
     </div>
   );
