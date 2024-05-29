fb5a79c31cfb7d6d11bfb28279f06608bbec2d0b
HyperOmerJ <omer.tapdiqov@gmail.com>
2024-05-30 00:02:37 +0400

    filterSort

--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./UserCard.css"
+import { MdFavorite } from "react-icons/md";
 
-const UserCard = ({user}) => {
+const UserCard = ({ user }) => {
   return (
-    <div className='col-4 pt-3 d-flex flex-column usercard'>
-      <p className='text-center fw-bold m-0'>{`${user.first_name} ${user.last_name}`}</p>
-      <p className='text-center'>{user.email}</p>
-      <img src={user.avatar} alt="" />
+    <div className="col-4 pt-3 d-flex flex-column usercard">
+      <p className="text-center fw-bold m-0">{`${user.first_name} ${user.last_name}`}</p>
+      <p className="text-center">{user.email}</p>
+      <img src={user.avatar} alt="avatar" />
+      <button className="mx-auto mt-3">
+        <p>Add Favorites</p>
+        <span>
+          <MdFavorite color="#88c7dc" />
+        </span>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
