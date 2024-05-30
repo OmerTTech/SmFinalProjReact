6605cdbe4e4e3dd5b093d5237f605a656963a8d6
Student <student@bii.edu.az>
2024-05-30 09:46:51 +0400

    updated details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import './App.css';
 import AdminRoutes from './Routes/AdminRoutes';
 import PublicRoutes from './Routes/PublicRoutes';
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserContext } from './Services/userContext';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Toaster } from "react-hot-toast";
 function App() {
   const { isLogged } = useContext(UserContext);
--- src/Routes/AdminRoutes.jsx
+++ src/Routes/AdminRoutes.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import AdminLayout from "../Layouts/AdminLayout";
 import Users from "../admin/Users/TotalUsers";
 import HomePage from "../admin/HomePage/HomePage";
+import { UserContext } from "../Services/userContext";
 
 const AdminRoutes = () => {
+  const { isLogged } = useContext(UserContext);
+  const navigator = useNavigate()
+  useEffect(() => {
+    if (isLogged) {
+      navigator("/admin/homepage");
+    }
+  }, [isLogged]);
   return (
     <AdminLayout>
       <Routes>
-        {/* Homepage 2-path for sidebar navlink */}
-        <Route path="/" element={<HomePage />} />
         <Route path="/admin/homepage" element={<HomePage />} />
-        {/* Homepage 2-path for sidebar navlink */}
-
         <Route path="/admin/users" element={<Users />} />
       </Routes>
     </AdminLayout>
